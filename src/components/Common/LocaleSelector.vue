0acf3690a4440572e58b285d07fba5744b64fda9
Aurélien Girault <me@agirault.com>
2022-06-19 22:19:52 +0200

    :sparkles: feat(i18n): create a locale selector component

---
+++ src/components/Common/LocaleSelector.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import LOCALES from '@/constants/locales.constants'
+import i18n from '@/i18n'
+
+const setLocale = (locale: string) => {
+  i18n.global.locale = locale
+}
+</script>
+
+<template>
+  <button
+    v-for="locale in LOCALES"
+    type="button"
+    class="text-slate-100 hover:text-slate-700 text-xs border-2 border-slate-100 hover:bg-slate-100 font-bold w-10 h-8 text-center mx-1 transition-all duration-200"
+    @click="setLocale(locale)"
+  >
+    {{ locale }}
+  </button>
+</template>
